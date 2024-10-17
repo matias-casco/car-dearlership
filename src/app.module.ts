48993598230a336bcbf73404febdd5a4d040e3cd
Matias Casco <kon-dcasco@bancobasa.com.py>
2024-10-17 01:29:52 -0300

    feat: add brands crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { CarsService } from './cars/cars.service';
 import { CarsController } from './cars/cars.controller';
 import { CarsModule } from './cars/cars.module';
+import { BrandsModule } from './brands/brands.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
-  imports: [CarsModule],
+  imports: [CarsModule, BrandsModule, SeedModule],
   controllers: [CarsController],
   providers: [CarsService],
 })
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,69 +1,67 @@
-import { CreateCarDto } from './dto/create-car.dto';
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
-import { Car } from './interfaces/car.interface';
+import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
 import { v4 as uuid } from 'uuid';
-import { UpdateCarDto } from './dto/update-car.dto copy';
+
+import { CreateCarDto, UpdateCarDto } from './dto';
+import { Car } from './interfaces/car.interface';
+import { CARS_SEED } from 'src/seed/data/cars.seed';
 
 @Injectable()
 export class CarsService {
-  private cars: Car[] = [
-    {
-      id: uuid(),
-      brand: 'Toyota',
-      model: 'Corolla',
-    },
-    {
-      id: uuid(),
-      brand: 'Honda',
-      model: 'Civic',
-    },
-    {
-      id: uuid(),
-      brand: 'Jeep',
-      model: 'Cherokee',
-    },
-  ];
 
-  findAll(): Car[] {
-    return this.cars;
-  }
+    private cars: Car[] = CARS_SEED;
 
-  findOneById(id: string) {
-    const car = this.cars.find((car) => car.id === id);
-    if (!car) throw new NotFoundException(`Car with id '${id}' not found`);
 
-    return car;
-  }
+    findAll() {
+        return this.cars;
+    }
 
-  create(createCarDto: CreateCarDto): Car {
-    const newCar = {
-      id: uuid(),
-      ...createCarDto,
-    };
+    findOneById( id: string ) {
+        
+        const car = this.cars.find( car => car.id === id );
+        if ( !car ) throw new NotFoundException(`Car with id '${ id }' not found`);
+        
+        return car;
+    }
 
-    this.cars.push(newCar);
-    return newCar;
-  }
+    create( createCarDto: CreateCarDto ) {
 
-  update(id: string, updateCarDto: UpdateCarDto): Car {
-    const car = this.findOneById(id);
+        const car: Car = {
+            id: uuid(),
+            ...createCarDto
+        }
 
-    if (updateCarDto.id && updateCarDto.id !== id)
-      throw new BadRequestException(
-        `Param id '${id}' does not match body id '${updateCarDto.id}'`,
-      );
+        this.cars.push( car );
 
-    const index = this.cars.indexOf(car);
-    this.cars[index] = { ...car, ...updateCarDto, id };
-    return this.cars[index];
-  }
+        return car;
+    }
 
-  delete(id: string) {
-    const car = this.findOneById(id);
-    this.cars = this.cars.filter((car) => car.id !== id);
-  }
+    update( id: string, updateCarDto: UpdateCarDto ) {
+
+        let carDB = this.findOneById( id );
+        
+        if( updateCarDto.id && updateCarDto.id !== id )
+            throw new BadRequestException(`Car id is not valid inside body`);
+
+        this.cars = this.cars.map( car => {
+
+            if ( car.id === id ) {
+                carDB = { ...carDB,...updateCarDto, id }
+                return carDB;
+            }
+
+            return car;
+        })
+        
+        return carDB;
+    }
+
+    delete( id: string ) {
+        const car = this.findOneById( id );
+        this.cars = this.cars.filter( car => car.id !== id );
+    }
+
+    fillCarsWithSeedData( cars: Car[] ) {
+        this.cars = cars;
+    }
+
 }
